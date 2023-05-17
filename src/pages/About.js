f3fd8df1bc84e5be8774a5ca5d7411a3a79e361b
Sven Axelsson <sven.axelsson1@learnet.se>
2023-05-17 11:12:00 +0200

    About us simple css

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,17 @@ function About() {
   return (
     <div className="about">
       <h1>Our story</h1>
-      <h4>Our company motto has always been to serve the cosumer first</h4>
+      <h4>
+        Our company motto has always been to serve the cosumer first..Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Orci a scelerisque purus
+        semper eget. Massa tincidunt dui ut ornare lectus sit amet est. Et
+        malesuada fames ac turpis. Tellus in metus vulputate eu scelerisque
+        felis. Orci dapibus ultrices in iaculis. Mus mauris vitae ultricies leo
+        integer malesuada. Sagittis purus sit amet volutpat consequat mauris
+        nunc. Elementum integer enim neque volutpat ac tincidunt vitae semper
+        quis. Rhoncus dolor purus non enim praesent elementum facilisis.
+      </h4>
       <div className="text">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
@@ -16,6 +26,10 @@ function About() {
         mollit anim id est laborum.{" "}
       </div>
       <h2>The company was founded on a dream of improving the world.</h2>
+      <p>
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+        officia deserunt mollit anim id est laborum.1997-2023
+      </p>
     </div>
   );
 }
