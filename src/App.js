9bebece64077d6d4f1096ecd73a5a76142203510
Sven Axelsson <sven.axelsson1@learnet.se>
2023-05-17 10:55:41 +0200

    About text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navbar from "./Componants/Navbar";
 import Home from "./pages/Home";
+import About from "./pages/About";
 import Footer from "./Componants/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
         </Routes>
         <Footer />
       </Router>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "../Style/About.css";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="about">
+      <h1>Our story</h1>
+      <h4>Our company motto has always been to serve the cosumer first</h4>
+      <div className="text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.{" "}
+      </div>
+      <h2>The company was founded on a dream of improving the world.</h2>
+    </div>
+  );
 }
 
 export default About;
