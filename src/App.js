94632647a1f0d0942a11c5f0f035773e3b9eff8a
Sven Axelsson <sven.axelsson1@learnet.se>
2023-05-17 12:42:51 +0200

    setting up produkt

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./Componants/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Footer from "./Componants/Footer";
+import Product from "./pages/Product";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/About" element={<About />} />
+          <Route path="/Product" element={<Product />} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/pages/Product.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../Style/Product.css"
+
+function Product() {
+  return (
+    <div>Product</div>
+  )
+}
+
+export default Product
